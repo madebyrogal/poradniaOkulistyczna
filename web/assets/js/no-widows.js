27cb2d00a2e18df170aeb63c4b712dd7c5dd2aee
madebyrogal <tomasz@madebyrogal.com>
2016-07-21 08:24:22 +0200

    FIX no-widow plugin fix html tag issue

--- web/assets/js/no-widows.js
+++ web/assets/js/no-widows.js
@@ -1,8 +1,8 @@
 $.fn.noWidows = function() {
     
     return this.each(function(i, elem) {
-        var str = $(elem).text();
-        $(elem).text(str.replace(/ ([^\s]){1} /gi, ' $1\xa0'));
+        var str = $(elem).html();
+        $(elem).html(str.replace(/ ([^\s]){1} /gi, ' $1\xa0'));
     });
     
 };
